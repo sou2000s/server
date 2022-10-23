5dd32b3dfef18a45e3004c751c119adfbb8366ae
netox222 <souparnamahato146@gmail.com>
2022-10-23 09:36:43 +0530

    hotels addeded

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT|| 5000
+app.use(cors())
+const hotels = require("./data/hotels.json")
+
+app.get("/" , (resq , res)=>{
+    res.send("servetr eunnin")
+})
+
+app.get("/hotels" , (req, res)=>{
+    res.send(hotels);
+})
+
+app.listen(port ,()=>{
+    console.log("running on por " , port);
+})
